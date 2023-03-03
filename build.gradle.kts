a76907e4d8ce1aeccff47a2ba3924f36d71c08a2
Daniel Garnier-Moiroux <git@garnier.wf>
2023-03-03 22:43:38 +0100

    empty boot project
    - The project uses Spring MVC + Thymeleaf, not Spring Security just yet.
    - We don't use Dev Tools because it keeps you logged-in between app
      restarts, which is a problem for demos.

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.3"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "wf.garnier.spring.security"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
