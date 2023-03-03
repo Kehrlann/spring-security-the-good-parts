fff6963a54d42744945aaaf84585c8f8c680f553
Daniel Garnier-Moiroux <git@garnier.wf>
2023-03-03 22:43:38 +0100

    empty boot project
    - The project uses Spring MVC + Thymeleaf, not Spring Security just yet.
    - We don't use Dev Tools because it keeps you logged-in between app
      restarts, which is a problem for demos.

---
+++ src/test/java/wf/garnier/spring/security/thegoodparts/TheGoodPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package wf.garnier.spring.security.thegoodparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGoodPartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
