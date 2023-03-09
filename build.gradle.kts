8a3999cc8352183d2856ea85bd87985e2f23d209
Daniel Garnier-Moiroux <git@garnier.wf>
2023-03-09 12:28:12 +0100

    Introduce OAuth2 login

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
