3abfd0ac188260ab5dbd9d8e49a0c5ff974b2db2
Daniel Garnier-Moiroux <git@garnier.wf>
2024-04-08 11:28:57 +0200

    docs: add slides

---
+++ slides/setup/shiki.ts
@@ -0,0 +1,14 @@
+/* ./setup/shiki.ts */
+import { defineShikiSetup } from '@slidev/types'
+
+export default defineShikiSetup(() => {
+  return {
+    themes: {
+      dark: 'dark-plus',
+      light: 'light-plus',
+    },
+    transformers: [
+      // ...
+    ]
+  }
+})
